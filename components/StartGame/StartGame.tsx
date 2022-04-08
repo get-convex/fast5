142f6d3f0fd4587f0324e9b7489f06e8e7310855
Mike Wheaton <mike@wheaton.dev>
2022-04-08 12:05:19 -0500

    Add instructions

--- components/StartGame/StartGame.tsx
+++ components/StartGame/StartGame.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useMutation } from '../../convex/_generated';
 import Button from '../Button/Button';
 import Modal from '../Modal/Modal';
+import styles from './StartGame.module.scss';
 
 function StartGame() {
   const router = useRouter();
@@ -36,15 +37,32 @@ function StartGame() {
 
   return (
     <Modal>
-      <div className="flex my-2">
+      <div className={styles.instructions}>
+        <p>
+          Fast5 is a word racing game. Play against a friend, or a friendly
+          Internet stranger!
+        </p>
+        <p>
+          Once the round starts, just start typing your guess, and then hit
+          enter to submit it. The first racer to guess the word each round adds
+          some points to their total score. Guessing earlier gets you more
+          points!
+        </p>
+        <p>
+          You can’t see exactly what letters the other person is guessing, but
+          you can see how close they’re getting. If you start to get worried
+          they’re going to win, you can steal their answers by typing an
+          exclamation point! This will let you see both your letters and theirs,
+          but the number of points you can win in that round will be half as
+          much as them.
+        </p>
+        <p>After 5 rounds, the racer with the most points wins!</p>
+      </div>
+      <div className={styles.actions}>
         <Button onClick={handleCreateGame}>
           Create a Game to Play a Friend
         </Button>
-      </div>
-      <div className="flex my-2">
         <Button onClick={handleJoinGame}>Join a Friend&rsquo;s Game</Button>
-      </div>
-      <div className="flex my-2">
         <Button onClick={handleRandom}>
           Play a Friendly Internet Stranger
         </Button>
