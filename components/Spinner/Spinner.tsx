573064de85199a34b7d4160b673ac75a8bd07452
Mike Wheaton <mike@wheaton.dev>
2022-04-19 09:26:50 -0500

    Add Spinner

---
+++ components/Spinner/Spinner.tsx
@@ -0,0 +1,24 @@
+import styles from './Spinner.module.scss';
+
+function Spinner() {
+  return (
+    <div className={styles.root}>
+      <div className={styles.spinner}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
