de72f6140b06e1b542e00b2bb196360d231f9246
Ian Macartney <ian@convex.dev>
2023-01-18 18:29:30 -0800

    protect against malicious round creation

--- convex/createRound.ts
+++ convex/createRound.ts
@@ -1,46 +1,51 @@
-import { mutation } from './_generated/server';
 import { WORDS } from '../lib/game/constants';
 import { Id } from './_generated/dataModel';
+import { mutationWithUser } from './common';
 
-export default mutation(async ({ db }, gameId: Id<'games'>, next: number) => {
-  var game = await db.get(gameId);
-  if (!game) throw Error('Game not found');
-  if (!game.ready) {
-    // Still waiting on the other party.
-    return;
-  }
-  if (game.rounds.length === 5) {
-    // Game is over.
-    return;
-  }
-  if (game.rounds.length !== next) {
-    // Lost the race to create the round.
-    return;
-  }
-  if (game.rounds.length > 0) {
-    const lastId = game.rounds[game.rounds.length - 1];
-    const lastRound = (await db.get(lastId))!;
-    if (typeof lastRound.winner !== 'number') {
-      // Last round, no one won yet!
+export default mutationWithUser(
+  async ({ db, user }, gameId: Id<'games'>, next: number) => {
+    var game = await db.get(gameId);
+    if (!game) throw Error('Game not found');
+    if (!user._id.equals(game.user1) && !user._id.equals(game.user2)) {
+      throw Error("User trying to start someone else's round");
+    }
+    if (!game.ready) {
+      // Still waiting on the other party.
       return;
     }
+    if (game.rounds.length === 5) {
+      // Game is over.
+      return;
+    }
+    if (game.rounds.length !== next) {
+      // Lost the race to create the round.
+      return;
+    }
+    if (game.rounds.length > 0) {
+      const lastId = game.rounds[game.rounds.length - 1];
+      const lastRound = (await db.get(lastId))!;
+      if (typeof lastRound.winner !== 'number') {
+        // Last round, no one won yet!
+        return;
+      }
+    }
+    const word = WORDS[Math.floor(Math.random() * WORDS.length)];
+    const round = {
+      word: word,
+      user1: {
+        guesses: [],
+        spying: false,
+      },
+      user2: {
+        guesses: [],
+        spying: false,
+      },
+      winner: null,
+      overflow: false,
+    };
+    const id = await db.insert('rounds', round);
+    game.rounds.push(id);
+    game.currentRound = game.rounds.length - 1;
+    await db.patch(game._id, game);
   }
-  const word = WORDS[Math.floor(Math.random() * WORDS.length)];
-  const round = {
-    word: word,
-    user1: {
-      guesses: [],
-      spying: false,
-    },
-    user2: {
-      guesses: [],
-      spying: false,
-    },
-    winner: null,
-    overflow: false,
-  };
-  const id = await db.insert('rounds', round);
-  game.rounds.push(id);
-  game.currentRound = game.rounds.length - 1;
-  await db.patch(game._id, game);
-});
+);
