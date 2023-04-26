d42b9309f8a0d6ba21663179dedf2b3cc7acb79c
Ian Macartney <ian@convex.dev>
2023-04-25 21:58:13 -0700

    no more zod

--- lib/game/proto.ts
+++ lib/game/proto.ts
@@ -1,32 +1,28 @@
-import { z } from 'zod';
+export interface UserStats {
+  wins: number;
+  losses: number;
+  ties: number;
+}
 
-const UserStatsZod = z.object({
-  wins: z.number(),
-  losses: z.number(),
-  ties: z.number(),
-});
-export type UserStats = z.infer<typeof UserStatsZod>;
+type GameUser = {
+  displayName: string;
+  photoUrl: string;
+  score: number;
+  isYou: boolean;
+  stats: UserStats;
+};
 
-const GameUser = z.object({
-  displayName: z.string(),
-  photoUrl: z.string(),
-  score: z.number(),
-  isYou: z.boolean(),
-  stats: UserStatsZod,
-});
-
-export const BackendGameZod = z.object({
-  round: z.number(),
-  user1: GameUser,
-  user2: GameUser,
-  public: z.boolean(),
-  ready: z.boolean(),
-  abandoned: z.boolean(),
-  inRound: z.boolean(),
-  winner: z.number(),
-  over: z.boolean(),
-});
-export type BackendGame = z.infer<typeof BackendGameZod>;
+export interface BackendGame {
+  round: number;
+  user1: GameUser;
+  user2: GameUser;
+  public: boolean;
+  ready: boolean;
+  abandoned: boolean;
+  inRound: boolean;
+  winner: number;
+  over: boolean;
+}
 
 export interface BackendRound {
   word: null | string;
